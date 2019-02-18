79ee548b5ce87b6de1e31862869d2aa6741cea60
Oliver Fleckenstein <Oliverfl@live.dk>
2019-02-18 21:50:00 +0100

    Setup of project

---
+++ src/components/Board.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as styles from './Board.scss';
+
+const Board = () => {
+  return <div className={styles.container} />;
+};
+
+export default Board;
---
+++ src/components/Tetris.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as styles from './Tetris.scss';
+
+const Tetris = () => (
+  <div className={styles.container}>
+    <h1>Tetris</h1>
+  </div>
+);
+
+export default Tetris;
