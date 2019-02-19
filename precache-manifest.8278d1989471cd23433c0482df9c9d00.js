cf6fb5e28b64a7095ebe2fa2f5401cd1a92233da
Oliver Fleckenstein <Oliverfl@live.dk>
2019-02-19 23:15:03 +0100

    Updates

---
+++ precache-manifest.8278d1989471cd23433c0482df9c9d00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0277f3ed11c442e22cbb",
+    "url": "/tetris/static/js/runtime~main.0277f3ed.js"
+  },
+  {
+    "revision": "8528f5010ab2daf4cd91",
+    "url": "/tetris/static/js/main.8528f501.chunk.js"
+  },
+  {
+    "revision": "0dbe246448204fe6f337",
+    "url": "/tetris/static/js/1.0dbe2464.chunk.js"
+  },
+  {
+    "revision": "8528f5010ab2daf4cd91",
+    "url": "/tetris/static/css/main.d9b79c39.chunk.css"
+  },
+  {
+    "revision": "8515b1a6088337f85763e75e2826fc5d",
+    "url": "/tetris/index.html"
+  }
+];
